dba08282cedd48a29d428e0db6f5351fa4fb145e
Pete Hamrick <pete.hamrick@gmail.com>
2021-08-03 13:19:03 -0700

    inital file structure setup

---
+++ src/ImageList.js
@@ -0,0 +1,13 @@
+// import React, { Component } from 'react';
+// // import ImageItem from './ImageItem.js';
+
+// class ImageList extends Component {
+//     state = {  }
+//     render() { 
+//         return ( 
+//             <ImageItem />
+//          );
+//     }
+// }
+ 
+// export default ImageList;
